cc65ddb09fbdeda912ff309156ecbfa6ed6b15e7
EmanuelCastro <emanuelcastro96@gmail.com>
2020-10-15 15:15:23 -0300

    server

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+console.log(path.join(__dirname, 'public'));
+
 app.get('/', (req, res)=>{
     res.sendFile('./src/index.html', {root : __dirname});
 });
 
-app.listen(2500, ()=>{
+app.listen(1500, ()=>{
     console.log('App listening on port');
 });
