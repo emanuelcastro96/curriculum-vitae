3c8a35f7dcf15fa300d7f60934a3770b7e5ecea8
EmanuelCastro <emanuelcastro96@gmail.com>
2020-10-17 16:37:19 -0300

    Changes on layout and style

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const path = require('path');
 
+const port = process.env.PORT;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-console.log(path.join(__dirname, 'public'));
 
 app.get('/', (req, res)=>{
     res.sendFile('./src/index.html', {root : __dirname});
 });
 
-app.listen(1500, ()=>{
-    console.log('App listening on port');
+app.listen(port, ()=>{
+    console.log('App listening on port '+ port);
 });
